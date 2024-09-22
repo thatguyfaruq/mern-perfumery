67782e2ae772c2bafdae3ca11f7a956a261d2ea0
Faruq Oluderu <faruqtemilola@gmail.com>
2024-09-22 15:26:30 +0100

    second commit

--- client/src/auth/Login.jsx
+++ client/src/auth/Login.jsx
@@ -1,38 +1,45 @@
 import React from 'react'
 import NavLogo from "../assets/perfumery-logo.svg";
+import FrameImg from "../assets/frame.png";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
     <>
       <main className="sign-in-container">
-        <nav className="d-flex gap-2 p-5">
-          <img src={NavLogo} alt="nav-logo" />
-          <h2>Perfume House</h2>
+        <nav className="">
+          <Link className='d-flex gap-2 p-5' to="/">
+            <img src={NavLogo} alt="nav-logo" />
+            <h2 className="text-white d-none d-lg-block">Perfume House</h2>
+          </Link>
         </nav>
 
-        <div className="form-container ">
-          <form>
-            <h2>Welcome Back</h2>
-            <p>Fill in your information to access your account.</p>
-
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter your Email" />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Remember Me" />
-            </Form.Group>
-
-            <Button variant="primary">Sign In</Button>
-          </form>
-        </div>
+        <section className="d-flex flex-column justify-content-center align-items-center">
+          <div className="form-container">
+            <form>
+              <h2>Welcome Back</h2>
+              <p>Fill in your information to access your account.</p>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter your Email" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Enter your password" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Remember Me" />
+              </Form.Group>
+              <p>Forgot Password</p>
+              <Button className="w-100" variant="primary">
+                Sign In
+              </Button>
+              <img src={FrameImg} alt="frame-img" />
+            </form>
+          </div>
+        </section>
       </main>
     </>
   );
