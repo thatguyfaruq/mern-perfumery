1a5cc695051580ab4b4c82dfb1521682c1b82c08
Faruq Oluderu <faruqtemilola@gmail.com>
2024-10-12 16:40:49 +0100

    latest class commit

--- server/index.js
+++ server/index.js
@@ -1,13 +1,31 @@
-import express from "express"
+import express from "express";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+import mongoose from "mongoose";
+import { connect } from "./db/db.js";
+import dotenv from "dotenv";
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+dotenv.config();
 
-app.listen(port, ()=> {
-  console.log(`http://localhost;${port}`);
-  
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "server is live" });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ errMsg: "route not found" });
+});
+
+connect()
+  .then(() => {
+    try {
+      app.listen(port, () => {
+        console.log(`http://localhost;${port}`);
+      });
+    } catch (error) {
+      console.log("can not connect to server" + error.message);
+    }
+  })
+  .catch((error) => {
+    console.log("invalid database coonection" + error.message);
+  });
